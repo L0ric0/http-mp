155b32c48f6dc9ff431943683deec65779fce4b1
Lorenz Steinert <lorenz@steinerts.de>
2020-12-21 20:37:44 +0100

    refactor Tokenizer::next_token and add more tests

--- http-mp/src/tokenizer.cpp
+++ http-mp/src/tokenizer.cpp
@@ -5,15 +5,26 @@
 // stl
 #include <string_view>
 
+using namespace std::literals;
+
 namespace http_mp {
 
 Tokenizer::Tokenizer(const std::string_view message) : m_message(message) {}
 
 Token Tokenizer::next_token() {
-  if (size_t sep_pos = m_message.find_first_of(" \t", m_pos);
+  if (size_t sep_pos = m_message.find_first_of(" \t\n", m_pos);
       sep_pos != std::string_view::npos) {
 
     if (sep_pos == m_pos) {
+      if (auto token = m_message.substr(sep_pos, 3);
+          token == "\n\r " or token == "\n\r\t") {
+        m_pos += 3;
+        return next_token();
+      } else if (auto token = m_message.substr(sep_pos, 2);
+                 sep_pos == m_pos and token == "\n\r") {
+        m_pos += 2;
+        return Token{token, m_pos == m_message.size() ? true : false};
+      }
       m_pos++;
       return next_token();
     }
--- tests/src/tokenizer.cpp
+++ tests/src/tokenizer.cpp
@@ -18,3 +18,28 @@ TEST(Tokenizer, singelToken) {
   ASSERT_TRUE(token.is_end);
   ASSERT_EQ(token.token, "asdf");
 }
+
+TEST(Tokenizer, endline) {
+  http_mp::Tokenizer t{"asdf\n\rfdsa"};
+  http_mp::Token token = t.next_token();
+  ASSERT_EQ(token.token, "asdf");
+  token = t.next_token();
+  ASSERT_EQ(token.token, "\n\r");
+  token = t.next_token();
+  ASSERT_EQ(token.token, "fdsa");
+  ASSERT_TRUE(token.is_end);
+}
+
+TEST(Tokenizer, endlineAtEnd) {
+  http_mp::Tokenizer t{"\n\r"};
+  http_mp::Token token = t.next_token();
+  ASSERT_TRUE(token.is_end);
+  ASSERT_EQ(token.token, "\n\r");
+}
+
+TEST(Tokenizer, linecontiuation) {
+  http_mp::Tokenizer t{"\n\r\t"};
+  http_mp::Token token = t.next_token();
+  ASSERT_TRUE(token.is_end);
+  ASSERT_EQ(token.token, "");
+}
